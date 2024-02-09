1ac9746726a5e2b59daa9298efb853cb95a07625
Felipe Soto <felipe.soto@antarasolutions.com>
2024-02-09 17:34:35 -0300

    se agrega contenido a web

--- js/main.js
+++ js/main.js
@@ -68,29 +68,7 @@
     });
 
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        dots: false,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
+ 
 
 
     // Portfolio isotope and filter
